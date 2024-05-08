997152b3a313078d560b46e58df14b46fc70f8cc
gusdl1992 <tlgusdl92@gmail.com>
2024-05-08 14:33:15 +0900

    스프링부트3 자바 백엔드 개발 책 연습

---
+++ boardSite_Practice/src/main/java/com/example/boardSite_Practice/BoardSitePracticeApplication.java
@@ -0,0 +1,18 @@
+package com.example.boardSite_Practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardSitePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardSitePracticeApplication.class, args);
+	}
+
+
+}
+/*
+	스프링부트3 자바 백엔드 개발 입문 책 활용 연습
+
+*/
